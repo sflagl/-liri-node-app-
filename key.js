20dc3e07a674c1d6c4fdffaaffd71d0d16ae60f5
Samantha <lovelychick1111@yahoo.com>
2018-02-16 23:41:29 -0500

    testing

--- key.js
+++ key.js
@@ -1,13 +1,64 @@
+var Spotify = require('node-spotify-api');
+var Twitter = require('twitter');
+
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
+
+var client = new Twitter({
+  consumer_key: 'GAFeWnEvSPdtjDxYtdHEJmrhY',
+  consumer_secret: 'twXH4Mbv9gJBnDFm92z1bWep3ocPf4CwuoNYFUPpOJW2VOBzgW',
+  access_token_key: '964346940719845377-iuljZ96mtfwhYBiz2ZO9a7gH891I40q',
+  access_token_secret: 'cAHD7CwFkG3pTErAbGe709OnCHuvWVkfMFEcpS6i3qCfh'
+});
+
+client.get(path, params, callback);
+client.post(path, params, callback);
+client.stream(path, params, callback);
+ 
+var params = {screen_name: 'nodejs'};
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+    console.log(tweets);
+  }
+});
+
+
+spotify
+  .request('https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx')
+  .then(function(data) {
+    console.log(data); 
+  })
+  .catch(function(err) {
+    console.error('Error occurred: ' + err); 
+});
+
+
+spotify
+  .search({ type: 'track', query: 'All the Small Things' })
+  .then(function(response) {
+    console.log(response);
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
+
+
 console.log('this is loaded');
 
 exports.twitter = {
-  consumer_key: process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  consumer_key: process.env.GAFeWnEvSPdtjDxYtdHEJmrhY,
+  consumer_secret: process.env.twXH4Mbv9gJBnDFm92z1bWep3ocPf4CwuoNYFUPpOJW2VOBzgW,
+  access_token_key: process.env.964346940719845377-iuljZ96mtfwhYBiz2ZO9a7gH891I40q,
+  access_token_secret: process.env.cAHD7CwFkG3pTErAbGe709OnCHuvWVkfMFEcpS6i3qCfh
 };
 
 exports.spotify = {
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
+  id: process.env.e9737a461d5f4dcebe8e6bc75ca9f265,
+  secret: process.env.a2ba81b66a8a40828c2a711da6d69438
 };
+
+// var spotify = new Spotify({
+//   id: <e9737a461d5f4dcebe8e6bc75ca9f265>,
+//   secret: <a2ba81b66a8a40828c2a711da6d69438>
+// });
+ 
--- liri.js
+++ liri.js
@@ -1,1 +1,8 @@
 require("dotenv").config();
+
+var request = require('request');
+request('http://www.omdbapi.com/?i=tt3896198&apikey=6c4af503', function (error, response, body) {
+  console.log('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  console.log('body:', body); // Print the HTML for the Google homepage.
+});
